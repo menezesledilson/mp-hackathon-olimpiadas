432b08a3bb512df020cea53a64734b9d45e0ed2c
menezesledilson <ledilsonmenezes@gmail.com>
2024-08-07 17:09:44 -0300

    correcao

---
+++ src/test/java/com/api/olympic/OlympicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.olympic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
